834912d74e0d3a14ff3ecbc78df1cc12dd878f9d
gleachkr <gleachkr@gmail.com>
2019-07-11 11:03:19 -0500

    Decouple construction and rendering

--- proof.js
+++ proof.js
@@ -1,7 +1,11 @@
 class ProofNode {
-    static input () {
+    static input (init) {
             var theInput = document.createElement("input")
-            theInput.setAttribute("style","width:1ch");
+            if (init) {
+                theInput.value = init;
+                theInput.setAttribute("style","width:" + init.length + "ch");
+            }
+            else theInput.setAttribute("style","width:1ch");
             theInput.addEventListener('input', function () {
                 theInput.setAttribute("style","width:" + theInput.value.length + "ch");
             });
@@ -10,46 +14,10 @@ class ProofNode {
 
     constructor(obj) { 
         this.forest = [];
+        this.labelContent = "";
         this.ruleContent = "";
         this.infoContent = "";
-        this.parentNode
-
-        this.forestElt = document.createElement("div");
-        this.forestElt.setAttribute("class","forest");
-
-        this.labelElt = document.createElement("div");
-        this.labelElt.setAttribute("class","label");
-
-        this.elt = document.createElement("div");
-        this.elt.setAttribute("class","node");
-        this.elt.appendChild(this.forestElt);
-        this.elt.appendChild(this.labelElt);
-
-        this.inputElt = ProofNode.input()
-        this.inputElt.addEventListener('keyup', (e) => {
-            if (e.code == "Enter" && e.ctrlKey) {
-                e.preventDefault()
-                var newNode = this.addChild()
-                if (this.forest.length == 1) {
-                    newNode.labelElt.lastChild.lastChild.focus();
-                } else {
-                    newNode.inputElt.focus()
-                };
-            } else if (e.code == "Enter") {
-                e.preventDefault()
-                var newNode = this.parentNode.addChild()
-                newNode.inputElt.focus();
-            } else if (e.code == "Backspace" && e.ctrlKey) {
-                this.remove()
-                this.parentNode.inputElt.focus();
-                this.parentNode.trigger("changed",true)
-            };
-            this.trigger("changed",true)
-        });
-
-        this.labelElt.appendChild(document.createElement("div"));
-        this.labelElt.appendChild(this.inputElt);
-        this.labelElt.appendChild(document.createElement("div"));
+        this.parentNode = null
 
         if (obj) {
             this.label = obj.label;
@@ -58,60 +26,132 @@ class ProofNode {
         };
     }
 
+    renderOn(target) {
+        var elt = document.createElement("div");
+
+        var forestElt = document.createElement("div");
+        elt.forestElt = forestElt
+        forestElt.setAttribute("class","forest");
+
+        elt.addRule = () => {
+            var childElt = forestElt.lastChild
+                if (childElt) {
+                var childLabel = childElt.lastChild
+                var ruleContainer = document.createElement("div");
+                var ruleInput = ProofNode.input(this.ruleContent);
+                elt.ruleElt = ruleInput;
+                ruleContainer.setAttribute("class","rule");
+                childLabel.removeChild(childLabel.lastChild);
+                ruleContainer.appendChild(ruleInput);
+                childLabel.appendChild(ruleContainer);
+                ruleInput.value = this.ruleContent;
+                ruleInput.addEventListener('input', () => {
+                    this.ruleContent = ruleInput.value
+                    this.trigger("changed",true);
+                });
+                this.on("ruleChanged", (r) => {
+                    ruleInput.setAttribute("style","width:" + ruleInput.value.length + "ch");
+                    labelElt.lastChild.setAttribute("style",
+                        "padding-right:" + (ruleInput.value.length * .6) + "ch");
+                });
+                this.on("infoChanged", (i) => {
+                    ruleInput.setAttribute("title", i);
+                });
+            }
+        }
+
+        var theChildElt = this.forest.map((n) => {return n.renderOn(forestElt)})[0]
+
+        if (this.forest.length > 0) { elt.addRule() }
+
+        var labelElt = document.createElement("div");
+        if (!this.parentNode)  labelElt.setAttribute("class","root") 
+        else labelElt.setAttribute("class","label");
+        
+        var inputElt = ProofNode.input(this.label)
+        this.on("labelChanged", (l) => {
+            inputElt.value = l;
+            inputElt.setAttribute("style","width:" + l.length + "ch");
+        });
+
+        inputElt.addEventListener('keyup', (e) => {
+            if (e.code == "Enter" && e.ctrlKey) {
+                e.preventDefault()
+                var newNode = this.addChild()
+                if (this.forest.length == 1) elt.ruleElt.focus();
+                else forestElt.firstChild.inputElt.focus();
+            } else if (e.code == "Enter") {
+                e.preventDefault();
+                var newNode = this.parentNode.addChild();
+                elt.parentElement.firstChild.inputElt.focus()
+            } else if (e.code == "Backspace" && e.ctrlKey) {
+                var parentElt = elt.parentElement.parentElement;
+                this.remove()
+                parentElt.inputElt.focus()
+                this.parentNode.trigger("changed",true)
+            };
+            this.trigger("changed",true)
+        });
+
+        elt.setAttribute("class","node");
+        elt.appendChild(forestElt);
+        elt.appendChild(labelElt);
+        labelElt.appendChild(document.createElement("div"));
+        labelElt.appendChild(inputElt);
+        labelElt.appendChild(document.createElement("div"));
+        elt.inputElt = inputElt;
+
+        this.on("removed", () => { 
+            var nodeAbove = elt.parentElement.parentElement
+            elt.parentElement.removeChild(elt);
+            if (this.parentNode.forest.length > 0) nodeAbove.addRule()
+        });
+
+        this.on("newChild", (child) => { 
+            child.renderOn(forestElt)
+            if (this.forest.length == 1) elt.addRule() 
+        });
+
+        target.prepend(elt);
+
+        return elt
+    }
+
     get info() { return this.infoContent };
 
     set info(i) { 
         this.infoContent = i;
-        if (this.forest.length > 0) {
-            this.forest[0].labelElt.lastChild.lastChild.setAttribute("title",i);
-        }
+        this.trigger("infoChanged", false, i)
     }
 
-    get label() { return this.inputElt.value };
+    get label() { return this.labelContent };
 
     set label(l) { 
-        var theInput = this.inputElt
-        theInput.value = l 
-        theInput.setAttribute("style","width:" + theInput.value.length + "ch");
+        this.labelContent = l;
+        this.trigger("labelChanged", false, l) 
     };
 
     get rule() { return this.ruleContent };
 
     set rule(r) { 
         this.ruleContent = r;
-        if (this.forest.length > 0) {
-            var ruleElt = this.forest[0].labelElt.lastChild.lastChild
-            ruleElt.value = r;
-            ruleElt.setAttribute("style","width:" + ruleElt.value.length + "ch");
-            this.labelElt.lastChild.setAttribute("style","padding-right:" + (ruleElt.value.length * .6) + "ch"); 
-        }
+        this.trigger("ruleChanged", false, r)
     };
 
     addChild(obj) {
         var child = new ProofNode(obj);
         child.parentNode = this;
         this.forest.push(child);
-        if (this.forest.length == 1) {
-            var ruleContainer = document.createElement("div");
-            var ruleInput = ProofNode.input();
-            ruleContainer.setAttribute("class","rule");
-            child.labelElt.removeChild(child.labelElt.lastChild);
-            ruleContainer.appendChild(ruleInput);
-            child.labelElt.appendChild(ruleContainer);
-            this.rule = this.rule;
-            ruleInput.addEventListener('input', () => {
-                this.ruleContent = ruleInput.value
-                this.trigger("changed",true);
-            });
-        }
-        this.forestElt.prepend(child.elt);
+        this.trigger("newChild", false, child);
         return child;
-    }
+    };
 
     remove() {
-        this.parentNode.forestElt.removeChild(this.elt);
-        this.parentNode.forest.splice(this.parentNode.forest.indexOf(this),1);
-    }
+        if (this.parentNode) {
+            this.trigger("removed",false);
+            this.parentNode.forest.splice(this.parentNode.forest.indexOf(this),1);
+        }
+    };
 
     toJSON() {
         return {
@@ -129,23 +169,21 @@ class ProofNode {
                 o.decorate(obj.forest[i]);
             }
             i++;
-        }
-    }
+        };
+    };
 
     replace(obj) {
         this.label = obj.label
         this.rule = obj.rule
         this.forest.map((n) => {n.remove()});
         obj.forest.map((o) => {this.addChild(o)});
-    }
+    };
 
     on(eventName, handler) {
       if (!this._eventHandlers) this._eventHandlers = {};
-      if (!this._eventHandlers[eventName]) {
-        this._eventHandlers[eventName] = [];
-      }
+      if (!this._eventHandlers[eventName]) this._eventHandlers[eventName] = [];
       this._eventHandlers[eventName].push(handler);
-    }
+    };
 
     off(eventName, handler) {
       let handlers = this._eventHandlers && this._eventHandlers[eventName];
@@ -154,21 +192,20 @@ class ProofNode {
         if (handlers[i] === handler) {
           handlers.splice(i--, 1);
         }
-      }
-    }
+      };
+    };
 
     trigger(eventName, bubble, ...args) {
-      if (bubble && typeof(this.parentNode) != "undefined") {
+      if (bubble && this.parentNode) {
           this.parentNode.trigger(eventName, bubble, args)
       }
       if (!this._eventHandlers || !this._eventHandlers[eventName]) return;
       this._eventHandlers[eventName].forEach(handler => handler.apply(this, args));
-    }
-}
+    };
+};
 
 class ProofRoot extends ProofNode {
     constructor(obj) {
         super(obj)
-        this.labelElt.setAttribute("class","root");
-    }
-}
+    };
+};
